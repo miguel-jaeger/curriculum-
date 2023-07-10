d62fae3a50f57bef6f3e7b4648792a024a661538
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-07-09 22:18:29 -0500

    change global info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Portafolio personal, usando la plantilla Astrofy';
+export const SITE_DESCRIPTION = 'Astrofy es una plantilla para portafolios de código abierto diseñada para Astro, a través de la cual  se pueden construir portafolios, blogs, curriculum y tiendas virtuales utilizando además TailwindCSS.';
